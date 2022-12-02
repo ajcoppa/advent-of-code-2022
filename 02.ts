f1ce960fbce88733bccd57ec776e02cd53df1a29
AJ Coppa <aj.coppa@gmail.com>
2022-12-02 00:54:16 -0500

    2**

--- 02.ts
+++ 02.ts
@@ -5,18 +5,50 @@ import { loadFromFile, sum } from "./lib";
 async function main() {
   const rounds: string[] = await loadFromFile("02-input.txt");
   console.log(`Part 1: ${partOne(rounds)}`);
+  console.log(`Part 2: ${partTwo(rounds)}`);
 }
 
 function partOne(rounds: string[]): number {
-  const scores: number[] = rounds.map(scoreRound);
+  const scores: number[] = rounds.map(scoreRoundOne);
   return sum(scores);
 }
 
-function scoreRound(round: string): number {
+function partTwo(rounds: string[]): number {
+  const scores: number[] = rounds.map(scoreRoundTwo);
+  return sum(scores);
+}
+
+function scoreRoundOne(round: string): number {
   const choices: Choice[] = round.split(" ").map(parseChoice);
   const theirChoice = choices[0];
   const myChoice = choices[1];
   return scoreChoice(myChoice) + scoreGame(theirChoice, myChoice);
+}
+
+function scoreRoundTwo(round: string): number {
+  const splitStr = round.split(" ");
+  const theirChoice: Choice = parseChoice(splitStr[0]);
+  const myGoal: Goal = parseGoal(splitStr[1]);
+  const myChoice: Choice = figureOutMyChoice(theirChoice, myGoal);
+  return scoreChoice(myChoice) + scoreGame(theirChoice, myChoice);
+}
+
+function figureOutMyChoice(theirChoice: Choice, myGoal: Goal): Choice {
+  if (myGoal === Goal.Draw) {
+    return theirChoice;
+  } else if (
+    (theirChoice === Choice.Rock && myGoal === Goal.Win) ||
+    (theirChoice === Choice.Scissors && myGoal === Goal.Loss)
+  ) {
+    return Choice.Paper;
+  } else if (
+    (theirChoice === Choice.Rock && myGoal === Goal.Loss) ||
+    (theirChoice === Choice.Paper && myGoal === Goal.Win)
+  ) {
+    return Choice.Scissors;
+  } else {
+    return Choice.Rock;
+  }
 }
 
 function parseChoice(str: string): Choice {
@@ -30,6 +62,16 @@ function parseChoice(str: string): Choice {
 
   console.error(`Error – unhandled input: ${str}`);
   process.exit(1);
+}
+
+function parseGoal(str: string): Goal {
+  if (str === "X") {
+    return Goal.Loss;
+  } else if (str === "Y") {
+    return Goal.Draw;
+  } else {
+    return Goal.Win;
+  }
 }
 
 function scoreChoice(choice: Choice) {
@@ -62,6 +104,12 @@ enum Choice {
   Rock,
   Paper,
   Scissors,
+}
+
+enum Goal {
+  Win,
+  Loss,
+  Draw,
 }
 
 main();
