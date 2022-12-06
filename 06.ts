4dfd1bd9cb0bc31a60dfa0f4a38e5228092920cb
AJ Coppa <aj.coppa@gmail.com>
2022-12-06 06:43:10 -0500

    6**

--- 06.ts
+++ 06.ts
@@ -5,10 +5,20 @@ import { loadFromFile } from "./lib";
 async function main() {
   const input: string[] = await loadFromFile("06-input.txt");
   console.log(`Part 1: ${partOne(input[0])}`);
+  console.log(`Part 2: ${partTwo(input[0])}`);
 }
 
 function partOne(input: string): number {
   const windowSize = 4;
+  const windows = windowsOfOverlappingElements(input.split(""), windowSize);
+  const firstUniqueWindowIndex = windows.findIndex((window) =>
+    containsUniqueElements(window)
+  );
+  return firstUniqueWindowIndex + windowSize;
+}
+
+function partTwo(input: string): number {
+  const windowSize = 14;
   const windows = windowsOfOverlappingElements(input.split(""), windowSize);
   const firstUniqueWindowIndex = windows.findIndex((window) =>
     containsUniqueElements(window)
